5eebaa321b66483cecbd77d0f7955349d2181df9
hin.phipat <phipat.limprasutr@allianz.com>
2020-12-17 12:22:50 +0700

    finish @lab 4

--- src/app/ajax/ajax.module.ts
+++ src/app/ajax/ajax.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
 import { PostsComponent } from '../ajax/posts/posts.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [PostsComponent],
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, FormsModule, ReactiveFormsModule],
   exports: [PostsComponent],
 })
 export class AjaxModule {}
--- src/app/ajax/posts/posts.component.ts
+++ src/app/ajax/posts/posts.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-posts',
@@ -8,14 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
   posts: any[];
+  form: FormGroup;
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient, private fb: FormBuilder) {
+    this.form = this.fb.group({
+      userId: '',
+      title: '',
+      body: '',
+    });
+  }
 
   ngOnInit(): void {
+    this.loadPost();
+  }
+
+  loadPost(): void {
+    this.posts = [];
     this.httpClient
       .get('https://jsonplaceholder.typicode.com/posts')
       .subscribe((r) => {
         this.posts = r as any[];
+      });
+  }
+
+  addPost(): void {
+    const newPost = this.form.value;
+    this.httpClient
+      .get('https://jsonplaceholder.typicode.com/posts', newPost)
+      .subscribe((r) => {
+        this.form.reset();
+        console.log('add post success!');
+        this.loadPost();
       });
   }
 }
